05b31987c1aede51ba6d12fb8dc266778576bbb0
Bily <117677485+BilyXdev@users.noreply.github.com>
2022-11-16 18:23:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let counterContent = document.querySelector("#counter-content")
+let addBtn = document.querySelector("#add-btn")
+let removeBtn = document.querySelector("#remove-btn")
+let resetBtn = document.querySelector("#reset-btn")
+let counter = 0
+
+addBtn.addEventListener("click", function addCounter() {
+    counter += 1
+    counterContent.textContent = counter
+})
+removeBtn.addEventListener("click",function removeCounter() {
+    counter -= 1
+    counterContent.textContent = counter
+})
+resetBtn.addEventListener("click", function resetCounter() {
+    counter -= counter
+    counterContent.textContent = counter
+})
